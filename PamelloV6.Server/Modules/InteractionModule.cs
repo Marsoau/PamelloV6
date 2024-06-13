6c2edc5bb4392c07d0a354cddf53fc9b1eb30055
Marsoau <koteika5063@gmail.com>
2024-06-13 15:49:38 +0300

    Basic interactions

---
+++ PamelloV6.Server/Modules/InteractionModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Interactions;
+using PamelloV6.Server.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PamelloV6.Server.Modules
+{
+	public class InteractionModule : InteractionModuleBase<SocketPamelloInteractionContext>
+	{
+		[SlashCommand("ping", "Check if bot is alive")]
+		public async Task Ping() {
+			await RespondAsync("Pong", ephemeral: true);
+		}
+	}
+}
