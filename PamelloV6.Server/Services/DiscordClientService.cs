8894649fecb66f058668174b977b9f4eca387a5c
Marsoau <koteika5063@gmail.com>
2024-06-13 19:07:05 +0300

    Discord Client Service

---
+++ PamelloV6.Server/Services/DiscordClientService.cs
@@ -0,0 +1,25 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PamelloV6.Server.Services
+{
+	public class DiscordClientService
+	{
+		public DiscordSocketClient[] DiscordClients;
+
+		public DiscordSocketClient MainDiscordClient {
+			get => DiscordClients[0];
+		}
+
+		public DiscordClientService(IServiceProvider services) {
+			DiscordClients = new DiscordSocketClient[1];
+
+			DiscordClients[0] = services.GetRequiredService<DiscordSocketClient>();
+		}
+	}
+}
