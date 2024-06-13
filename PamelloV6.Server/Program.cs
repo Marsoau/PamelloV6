6399d4b3124faab1456cedc07cf45da3dfc5a7f5
Marsoau <koteika5063@gmail.com>
2024-06-13 14:49:39 +0300

    Empty bot startup

--- PamelloV6.Server/Program.cs
+++ PamelloV6.Server/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace PamelloV6.Server
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PamelloV6.Server
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+	public class Program
+	{
+		public static Task Main() => new Program().MainAsync();
+		public async Task MainAsync()
+		{
+			var hostBuilder = Host.CreateDefaultBuilder();
+
+			hostBuilder.ConfigureServices(services => {
+				services.AddSingleton(x => new DiscordSocketClient(
+					new DiscordSocketConfig() {
+						GatewayIntents = GatewayIntents.All,
+						AlwaysDownloadUsers = true
+					}
+				));
+			});
+
+			await RunPamelloAsync(hostBuilder.Build());
+		}
+
+		public async Task RunPamelloAsync(IHost host) {
+			using var serviceScope = host.Services.CreateScope();
+			var provider = serviceScope.ServiceProvider;
+
+			await RunBotsAsync(provider);
+
+			await Task.Delay(-1);
+		}
+
+		public async Task RunBotsAsync(IServiceProvider provider) {
+			var MainDiscordClient = provider.GetRequiredService<DiscordSocketClient>();
+
+			MainDiscordClient.Log += async (message) => {
+				Console.WriteLine(message);
+			};
+
+			await MainDiscordClient.LoginAsync(TokenType.Bot, "MTI1MDc2MzM0NjcxNDY5MzYzMg.GqF3b4.OVu84ru-0_-RtKUwcrQchAppjZgxaHUgnu_5yw");
+			await MainDiscordClient.StartAsync();
+
+		}
+	}
 }
