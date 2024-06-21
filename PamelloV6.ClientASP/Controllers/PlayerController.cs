1598c44f7f6fc489a75af8b9e47ade4edbdffd3b
Marsoau <koteika5063@gmail.com>
2024-06-21 22:55:26 +0300

    Player addition started

---
+++ PamelloV6.ClientASP/Controllers/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PamelloV6.ClientASP.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index() {
+            Console.WriteLine($"ckiezz: {Request.Cookies["token"]}");
+            return View();
+        }
+    }
+}
