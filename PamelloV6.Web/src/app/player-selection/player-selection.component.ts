842b983906b3294aa75d6866d0d1ecb450af8288
Marsoau <koteika5063@gmail.com>
2024-08-06 12:18:36 +0300

    New player select

--- PamelloV6.Web/src/app/player-selection/player-selection.component.ts
+++ PamelloV6.Web/src/app/player-selection/player-selection.component.ts
@@ -3,11 +3,12 @@ import { PamelloV6API } from '../../services/api/pamelloV6API.service';
 import { IPamelloPlayer } from '../../services/api/model/PamelloPlayer';
 import { SearchResult } from '../../services/api/pamelloV6DataAPI';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-player-selection',
 	standalone: true,
-	imports: [FormsModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './player-selection.component.html',
 	styleUrl: './player-selection.component.scss'
 })
@@ -15,17 +16,19 @@ export class PlayerSelectionComponent {
 	api: PamelloV6API;
 
 	inputValue: string;
-	selectValue: string;
 	selectionMode: boolean;
 	availablePlayers: IPamelloPlayer[];
+
+	displaySelect: boolean;
 
 	constructor(api: PamelloV6API) {
 		this.api = api;
 
 		this.inputValue = "";
-		this.selectValue = "";
 		this.selectionMode = true;
 		this.availablePlayers = [];
+
+		this.displaySelect = false;
 
 		this.api.events.PlayerCreated = async (newPlayerId: number) => {
 			let player = await this.api.data.GetPlayer(newPlayerId);
@@ -39,22 +42,19 @@ export class PlayerSelectionComponent {
 		this.LoadAwailablePlayers();
 	}
 
-	public GetSelectValue() {
-		if (this.selectValue != (this.api.selectedPlayer?.id ?? "none").toString()) {
-			this.selectValue = (this.api.selectedPlayer?.id ?? "none").toString();
-		}
-		return this.selectValue;
-	}
-
 	public async LoadAwailablePlayers() {
 		let searchResult = await this.api.data.SearchPlayers(0, 100);
 		if (!searchResult) return;
 
 		this.availablePlayers = searchResult.results;
-		this.UpdatePlayerSelectOption();
 	}
 
-	public async PlayerCreateClick() {
+	public async SwitchMode() {
+		this.selectionMode = !this.selectionMode;
+		this.displaySelect = false;
+	}
+
+	public async PlayerCreate() {
 		if (this.inputValue.length == 0) return;
 
 		let newPlayerId = await this.api.commands.PlayerCreate(this.inputValue) as number | null;
@@ -62,25 +62,18 @@ export class PlayerSelectionComponent {
 		this.selectionMode = true;
 		this.api.commands.PlayerSelect(newPlayerId);
 	}
-	public SelectionChanged() {
-		let valueNumber;
-		if (this.selectValue == "none") {
-			valueNumber = null;
-		}
-		else {
-			valueNumber = parseInt(this.selectValue);
-		}
-
-		this.UpdatePlayerSelectOption();
-	
-		this.api.commands.PlayerSelect(valueNumber);
+	public async PlayerDelete(playerId: number | null) {
+		
 	}
-	public UpdatePlayerSelectOption() {
-		if (!this.api.selectedPlayer) {
-			this.selectValue = "none";
+
+	public async SelectPlayer(playerId: number | null) {
+		this.displaySelect = false;
+
+		if (this.api.selectedPlayer?.id == playerId) {
+			await this.api.commands.PlayerSelect(null);
 		}
 		else {
-			this.selectValue = this.api.selectedPlayer.id.toString();
+			await this.api.commands.PlayerSelect(playerId);
 		}
 	}
 }
